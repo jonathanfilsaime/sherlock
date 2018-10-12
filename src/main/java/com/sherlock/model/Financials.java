1ccfe068fa5d086cc51e3901accd38912996fdc6
plp2675 <jonathan.filsaime@usaa.com>
2018-10-12 12:03:06 -0500

    merge

--- src/main/java/com/sherlock/model/Financials.java
+++ src/main/java/com/sherlock/model/Financials.java
@@ -29,4 +29,5 @@ public class Financials {
     private Double cashFlow;
     private String operatingGainsLosses;
 
+
 }
--- src/main/java/com/sherlock/model/FinancialsResponseObject.java
+++ src/main/java/com/sherlock/model/FinancialsResponseObject.java
@@ -3,10 +3,16 @@ package com.sherlock.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 @NoArgsConstructor
 public class FinancialsResponseObject {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String symbol;
     private Financials[] financials;
 
